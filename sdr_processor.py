bab744fbff0d361cab77c09f6ea8d2b1e3144680
Taylor2718 <104439423+Taylor2718@users.noreply.github.com>
2023-12-08 14:37:28 +0000

    Add files via upload

--- sdr_processor.py
+++ sdr_processor.py
@@ -22,106 +22,11 @@ bands = [(10.705,10.945),
          (11.705,11.945),
          (11.955,12.195),
          (12.205,12.445),
-         (12.455,12.695)]
-                                                        
-#twitter = True
-#%%
-def gen_weights(ns, ipad, cosb):
-    wei = np.zeros(ns)
-    wei[ipad+1:] = 1.0
-    
-    x = np.arange(cosb)/cosb*2*np.pi
-    c = (1-np.cos(x))/2.0
-    wei[ipad+1:ipad+1+cosb] = c + 0.05
-    wei[-cosb:] = 1-c + 0.05
-    
-    return wei
-
-
-def get_scan(pf):
-    global date
-
-    line = pf.readline()
-    w = line.split(',')
-
-    if len(w)==1:
-        return[0], 0
-    
-    scan = np.array(w[6:]).astype(float)
-    date = f"{w[0]}{w[1]}"
-    
-    return scan, date
-                         
-#%%Loading data
-infile = 'Transits/231130/00/HackRF_231130_0000.npy'
-pf = open(infile,'rb')
-w = np.load(pf,allow_pickle=True)
-
-lo = float(w[3])     # LO freq tagged on end
-nu = w[1]/1e6        # Frequency array in MHz
-spec = w[2]          # Band amplutudes in dB
-nu_start = nu[0]     # Keep start freq as reference 
-start = w[0] 
-
-ipos = np.argsort(nu)    # Reorder interstepped data
-nu = nu[ipos]
-spec = spec[ipos]
-nu_start = nu[0]     # Keep start freq as reference    
-nspec = len(spec)
-#wei = gen_weights(nspec,40,100)  # Generate weighting function
-
-nu_lo = nu[0] + 10782.72 #10410     # Lowest freq of low band
-nu_hi = nu[-1] + 10782.72 #10410   # Highest freq of high band
-nu_step = nu[1]-nu[0]    # Frequency step
-
-nsamp = int((nu_hi-nu_lo)/nu_step) # Array size for combined spec
-specs = []    # Initize list for combined spectra
-dates = []
-
-not_EOF = True
-while not_EOF:
-    scan = np.zeros(nsamp+1)
-    hit = np.zeros(nsamp+1)
-    
-    for i in range(5):  # Combine 4 scans ~1s
-        try:
-            w = np.load(pf,allow_pickle=True)
-        except:
-            not_EOF = False
-            break
-
-        date = w[0]          # Store date for check
-        lo = float(w[3])     # LO freq tagged on end
-        nu = w[1]/1e6        # Freq array
-        spec = w[2]          # Amp array
-        ips = np.argsort(nu) # De-jitter data   
-        nu = nu[ips]
-        spec = spec[ips]
-       
-        ipos = ((lo + nu - nu_lo)/nu_step).astype(int) # position output arr
-        ip = ((nu-nu[0])/nu_step).astype(int)          # position input arr
-
-#       print(hit.shape,wei.shape)
-        hit[ipos] = hit[ipos] + 1 #wei[ip]                # Weighted sum
-        scan[ipos] = scan[ipos] + spec  #*wei[ip]
-
-    #ipos = np.where(hit>0)[0]  # Just use data where there are hits
-    #scan[ipos] /= hit[ipos]
-        dates.append(date)   
-        specs.append(scan)         # Add spec
-    
-end = w[0]
-    
-specs = np.array(specs)        # Convert to array
-base = np.median(specs,axis=0) # Median baseline over period
-rain = np.mean(specs,axis=1)
-waterfall = specs-base         # Data for waterfall plot
-waterfall = waterfall[:-1]
-nu = nu + 9750
-#%%
+         (12.455,12.695)]                                                  
+#%%Reading data
 
 start_datetime = datetime.datetime(2023, 12, 2, 0)  # Start of analysis
-end_datetime = datetime.datetime(2023, 12, 2, 4)  # End of analysis
+end_datetime = datetime.datetime(2023, 12, 2, 1)  # End of analysis
 directory = "Transits" #Set to your local transits folder
 
 def format_datetime_string_directory(dt):
@@ -195,7 +100,6 @@ while current_datetime <= end_datetime:
                         ipos = ((lo + nu - nu_lo)/nu_step).astype(int) # position output arr
                         ip = ((nu-nu[0])/nu_step).astype(int)          # position input arr
 
-                #       print(hit.shape,wei.shape)
                         hit[ipos] = hit[ipos] + 1 #wei[ip]                # Weighted sum
                         scan[ipos] = scan[ipos] + spec  #*wei[ip]
 
@@ -205,13 +109,6 @@ while current_datetime <= end_datetime:
                         dates.append(date)   
                         specs.append(scan)         # Add spec
                     
-                end = w[0]
-                    
-                #specs = np.array(specs)
-                # Convert to array
-                #base = np.median(specs_file,axis=0) # Median baseline over period
-                #waterfall = specs_file-base         # Data for waterfall plot
-                #waterfall = waterfall[:-1]
                 nu = nu + 9750
     # Move to the next hour
     current_datetime += datetime.timedelta(hours=1)
@@ -224,14 +121,16 @@ def format_id(date):
 
 # Create a list of IDs
 ids = [format_id(date) for date in dates]
-#%%
+#%%Creating waterfall
 specs = np.array(specs)
 base = np.median(specs,axis=0)
-waterfall = specs-base
 #%%
+
+waterfall = specs-base
+
+#%%Plotting waterfall
 vmin = np.percentile(waterfall, 5)
 vmax = np.percentile(waterfall, 95)
-
 
 plt.figure(figsize=(10, 6))
 plt.imshow(waterfall, aspect='auto', cmap='viridis',  vmin=vmin, vmax=vmax)
@@ -250,37 +149,25 @@ plt.ylabel('Time')
 plt.title('Waterfall Plot')
 plt.show()
 
-#%%
+#%%Plotting mean waterfall (collapsing time)
 mean_waterfall = np.mean(waterfall,axis=0)
 sem_waterfall = sem(waterfall,axis=0)
+max_waterfall = np.max(waterfall,axis=0)
+min_waterfall = np.min(waterfall,axis=0)
 
-plt.plot(nu,mean_waterfall)
+plt.plot(nu,mean_waterfall, '--', color = 'black', label = 'Mean')
+plt.plot(nu,min_waterfall, color = 'blue', label = 'Min Hold')
+plt.plot(nu,max_waterfall, color = 'red', label = 'Max Hold')
+
 #plt.fill_between(nu, mean_waterfall - sem_waterfall, mean_waterfall + sem_waterfall, color='blue', alpha=0.1, label='SEM')
 plt.xlabel("Frequency (MHz)")
 plt.ylabel("Relative Amplitude (a.u.)")
+plt.legend()
 plt.grid()
 
-#%%
-base_collapse = np.median(specs,axis=0) # Median baseline over period
-
-mean_specs = np.mean(specs,axis=0)
-sem_specs = sem(specs,axis=0)
-
-spec_collapse = mean_specs - base_collapse
-
-plt.plot(nu,mean_specs)
-plt.fill_between(nu, mean_specs - sem_specs, mean_specs + sem_specs, color='blue', alpha=0.1, label='SEM')
-plt.xlabel("Frequency (MHz)")
-plt.ylabel("Relative Amplitude (a.u.)")
-plt.grid()
-#%%
-plt.plot(nu,spec_collapse)
-plt.xlabel("Frequency (MHz)")
-plt.ylabel("Relative Amplitude (a.u.)")
-plt.grid()
-#%%
-lower_1 = 10000  
-upper_1 = 10400  
+#%%Plotting waterfall for bands (collapisng frequency)
+lower_1 = 11650
+upper_1 = 11800  
 
 # Combine conditions to extract columns within the specified range
 mask_1 = (nu > lower_1) & (nu < upper_1)
@@ -288,30 +175,43 @@ specs_1 = specs[:, mask_1]
 waterfall_1 = waterfall[:, mask_1]
 nu_1 = nu[mask_1]
 mean_waterfall_1 = np.mean(waterfall_1,axis=1)
-mean_specs_1 = np.mean(specs_1,axis=1)
+mean_specs_1 = np.mean(specs_1,axis=0)
 
-plt.plot(dates, mean_specs_1)
-#%%
-vmin = np.percentile(waterfall, 20)
-vmax = np.percentile(waterfall, 80)
+mask = mean_waterfall_1 < -1
+dates = np.array(dates)
+# Apply the mask to filter out unwanted indices
+filtered_dates = dates[~mask]
+filtered_mean_waterfall = mean_waterfall_1[~mask]
 
+plt.plot(filtered_dates, filtered_mean_waterfall)
 
-plt.figure(figsize=(10, 6))
-plt.imshow(waterfall, aspect='auto', cmap='viridis',  vmin=vmin, vmax=vmax)
+#%%Plotting Fourier transform
+from scipy.signal import hann
 
-# Set x-axis ticks and labels based on the frequency array 'nu'
-num_ticks = 6  # Adjust the number of ticks as needed
+time_step = np.diff(filtered_dates).mean().total_seconds()
+fft_result = np.fft.fft(filtered_mean_waterfall)
 
-ticks_positions = np.linspace(0, len(nu) - 1, num_ticks)
-ticks_labels = np.linspace(10000, 12000, num_ticks)
+exp = np.log10(np.max(np.abs(fft_result)))/5
+#fft_result_shifted = np.fft.fftshift(fft_result)
+windowed_data = hann(len(filtered_mean_waterfall)) * filtered_mean_waterfall
 
-plt.xticks(ticks_positions, ticks_labels)
+# Calculate the frequency values
+frequencies = np.fft.fftfreq(len(fft_result), d=time_step)
+white_noise = np.ones_like(frequencies)
+one_over_f_noise = 1 / (frequencies)**(1)
+one_over_f_noise[0] = 0  # Avoid division by zero
+# Plot the magnitude spectrum with a logarithmic scale for the frequency axis
+plt.plot(frequencies, np.abs(fft_result), label='FFT')
+plt.plot(frequencies, white_noise, label='White Noise', linestyle='--')
+plt.plot(frequencies, one_over_f_noise, label='1/f Noise', linestyle='--')
 
-plt.colorbar(label='Amplitude')
-plt.xlabel('Frequency')
-plt.ylabel('Time')
-plt.title('Waterfall Plot')
+plt.title('Fourier Transform of Relative Amplitude Data')
+plt.xlabel('Frequency (Hz)')
+plt.ylabel('Amplitude')
+plt.xscale('log')  # Use a logarithmic scale for the frequency axis
+plt.yscale('log')  # Use a logarithmic scale for the frequency axis
+plt.grid(which='both', axis='both', linestyle='--', linewidth=0.5)  # Add a grid with dashed lines
+plt.legend()
 plt.show()
-
 
 
